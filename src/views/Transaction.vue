998379375fd37ba4cbd008d601c3693414e5bee9
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-03-30 05:34:59 +0700

    Add transaction indicators

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -1,21 +1,32 @@
 <template>
   <v-container fluid id="transaction">
+    <div id="transaction__success" v-if="isSuccess">
+      <h1>Transaksi Berhasil!</h1>
+      <img src="https://png.pngtree.com/svg/20170706/4631f6529c.png" alt="">
+      <router-link to="/">
+        <button class="btn_box">Kembali ke Home</button>
+      </router-link>
+    </div>
     <h1 id="order__header">Transaction #{{this.orderId}}</h1>
     <v-radio-group v-model="payment">
       <v-radio label="Pembayaran Tunai" value="tunai"></v-radio>
       <p>Lakukan pembayaran jasa secara tunai. Sangat disarankan untuk menggunakan layanan BRI karena keamaan lebih
         terjamin</p>
       <v-radio label="ATM BRI" value="bri"></v-radio>
-      <p>Lakukan pembayaran melalui ATM BRI. Silahkan transfer sejumlah</p>
-      <h2>{{ order.total_price }}</h2>
-      <p>Ke rekening berikut</p>
-      <h2>888801000157508</h2>
-      <v-text-field label="No Refferal" v-model="referral"></v-text-field>
+      <p>Lakukan pembayaran melalui ATM BRI.</p>
+      <div v-if="payment === 'bri'">
+        <p>Silahkan transfer sejumlah</p>
+        <h2>{{ order.total_price }}</h2>
+        <p>Ke rekening berikut</p>
+        <h2>888801000157508</h2>
+        <v-text-field label="No Refferal" v-model="referral"></v-text-field>
+      </div>
     </v-radio-group>
     <v-progress-circular class="center" :indeterminate="true" v-if="isLoading"></v-progress-circular>
     <button class="btn_box" @click="submit" v-else>
       {{ this.payment === 'bri' ? 'Cek Pembayaran' : 'Selesai' }}
     </button>
+    <p v-if="isWrong" id="indicator">Kode referral salah! Ulangi lagi.</p>
   </v-container>
 </template>
 
@@ -28,19 +39,22 @@
         orderId: -1,
         payment: '',
         referral: '',
+        isSuccess: false,
+        isWrong: false
       };
     },
     methods: {
       submit() {
         if (this.payment === 'tunai') {
           // Transaction Success
+          this.isSuccess = true;
         } else {
           this.$store.dispatch('transaction/fetchStatus', {
             referral: this.referral, orderId: this.orderId, callback: (status) => {
               if (status === 'paid') {
-
+                this.isSuccess = true;
               } else {
-
+                this.isWrong = true;
               }
             },
           });
@@ -49,6 +63,7 @@
     },
     mounted() {
       this.orderId = this.$route.params.id;
+      this.$store.dispatch('transaction/fetchOrder', { id: this.orderId }, { root: true });
     },
     computed: {
       order() {
@@ -61,6 +76,55 @@
   };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+#transaction {
+  padding: 4vh !important;
+  padding-top: 8vh !important;
 
+  #order__header {
+    font-family: 'Playfair Display';
+    font-weight: normal;
+  }
+
+  button {
+    width: 100%;
+  }
+
+  #indicator {
+    color: red;
+    text-align: center;
+    margin-top: 1vh;
+  }
+
+  #transaction__success {
+    position: absolute;
+    z-index: 1001;
+    background-color: white;
+    min-height: 100vh;
+    width: calc(100% - 8vh);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+      margin-top: 10vh;
+      font-family: 'Playfair Display';
+      font-weight: normal;
+      text-align: center;
+    }
+
+    img {
+      margin-top: 3vh;
+      width: calc(100% - 8vh);
+    }
+
+    a {
+      width: 100%;
+      color: black;
+      button {
+        font-size: 2.5vh;
+      }
+    }
+  }
+}
 </style>
