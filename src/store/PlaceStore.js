7717311f5b2f6f5e910092ada19a52cc266d3e09
gerywahyunugraha <gerywahyunugraha@gmail.com>
2019-03-29 11:22:02 +0700

    Add helper function is place store

--- src/store/PlaceStore.js
+++ src/store/PlaceStore.js
@@ -3,22 +3,37 @@ import { url } from '../config';
 const PlaceStore = {
   state: {
     places: [],
+    status: "pending" | "idle",
+    currentPlace: {},
   },
   mutations: {
     setPlaces(state, places) {
       state.places = places;
+    },
+    setStatus(state, status) {
+      state.status = status;
+    },
+    setCurrentPlace(state, place) {
+      state.place = place;
     }
   },
   actions: {
     fetchAll({ commit }) {
+      commit('setStatus', "pending");
       fetch(`${url}/place`)
           .then(response => response.json())
-          .then(data => commit('setPlaces', data))
+          .then(data => {
+            commit('setPlaces', data);
+            commit('setStatus', "idle");
+          });
     }
   },
   getters: {
     getPlacesByType: (state) => (placeType) => {
       return state.places.find(place => place.place_type === placeType);
+    },
+    isLoading: (state) => {
+      return state.status === "pending";
     }
   }
 };
