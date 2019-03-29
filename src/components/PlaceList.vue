d4a8fad7ae32d0964d7f7f54458bb6d63363c732
gerywahyunugraha <gerywahyunugraha@gmail.com>
2019-03-29 22:54:29 +0700

    Fix refresh

--- src/components/PlaceList.vue
+++ src/components/PlaceList.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container class="placelist">
     <v-progress-circular
-      :indeterminate="true"
-      v-if="isLoading"
+        :indeterminate="true"
+        v-if="isLoading"
     />
     <div
-      v-else
-      class="placegrid"
+        v-else
+        class="placegrid"
     >
       <place
-        v-for="place in places"
-        :key="place.id"
-        :place="place"
+          v-for="place in places"
+          :key="place.id"
+          :place="place"
       />
     </div>
   </v-container>
@@ -19,39 +19,38 @@
 
 
 <script>
-import Place from './Place';
+  import Place from './Place';
 
-export default {
-  name: 'PlaceList',
-  computed: {
-    places() {
-      return this.$store.state.placeList.places;
+  export default {
+    name: 'PlaceList',
+    computed: {
+      places() {
+        return this.$store.state.placeList.places;
+      },
+      isLoading() {
+        return this.$store.getters['placeList/isLoading'];
+      },
     },
-    isLoading() {
-      return this.$store.getters['placeList/isLoading'];
-    },
-  },
-  beforeMount() {
-    if (!this.$store.getters['placeList/hasLoad']) {
+    beforeMount() {
       this.$store.dispatch('placeList/fetchAll', {}, {root: true});
-    }
-  },
-  components: {
-    Place,
-  },
-};
+    },
+    components: {
+      Place,
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.placelist {
-  margin-top: 2vh !important;
-}
-
-.placegrid {
-  overflow: auto;
-  white-space: nowrap;
-  span {
-    display: inline-block;
+  .placelist {
+    margin-top: 2vh !important;
   }
-}
+
+  .placegrid {
+    overflow: auto;
+    white-space: nowrap;
+
+    span {
+      display: inline-block;
+    }
+  }
 </style>
