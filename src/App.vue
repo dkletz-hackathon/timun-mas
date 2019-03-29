41650f595b26cf3354d82a69d91a12dd552bc0e6
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-03-29 14:36:23 +0700

    Create base navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <navbar />
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +8,18 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/partials/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  }
+}
+</script>
+
 
 <style lang="scss">
 @import 'assets/styles/index';
